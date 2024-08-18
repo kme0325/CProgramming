fa4a2e45311683be6af3dd8cdb9f1555de09cc87
kme0325 <kusj2002@gmail.com>
2024-08-18 11:57:00 +0900

    2024-08-18
    2024-08-18 C언어 github 테스트

---
+++ Project1/Source09.c
@@ -0,0 +1,50 @@
+#include "Header.h"
+
+//첫날에는 1월을 가지고 있습니다.
+//이후 30일 동안 전날보다 2배의 금액을 받게 됩니다.
+//30일이 되었을 때 총 금액이 얼마나 나오는지 출력하세요.
+//1+2=3,2+4=6
+//int main()
+//{
+	/*int i = 1;
+	int total = 0;
+
+	for (int d = 1; d < 30; d++)
+	{
+		total = total + i;
+		i = total * 2;
+		printf("%d일차 금액 i = %d\n", d, total);
+	}
+	printf("%d원\n", i);*/
+
+	////입력한 숫자가 소수인지를 판단하는 프로그램
+	////조건) int is_prime이라는 변수가 제공
+	////		이 is_prime이 1일 경우 소수, 0일 경우 소수가 아니라고 출력해줌
+	//int n, is_prime;
+	//printf("숫자를 입력해주세요 >> ");
+	//scanf("%d", &n);
+	//is_prime = 1; //핵심
+	//for (int i = 1; i <= n; i++)
+	//{
+	//	if (i == 1 || i == n)
+	//	{
+	//		continue;
+	//	}
+	//	else if (n % i == 0)
+	//	{
+	//		is_prime = 0;
+	//		break;
+	//	}
+	//}
+	//if (is_prime == 1)
+	//{
+	//	printf("%d은(는) 소수입니다.\n", n);
+	//}
+	//else
+	//{
+	//	printf("%d은(는) 소수가 아닙니다.\n", n);
+	//}
+
+	//return 0;
+//}
+
